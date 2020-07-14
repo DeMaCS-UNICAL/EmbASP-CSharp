4b9e8f20a776dd7ee83658f74d72403d115f5750
Giorgio Andronico <bytecodex2@gmail.com>
2020-07-14 19:00:30 +0200

    Added support for IDLV.

---
+++ it/unical/mat/test/specialization/idlv/UnweightedEdge.cs
@@ -0,0 +1,50 @@
+﻿using it.unical.mat.embasp.languages;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace it.unical.mat.test
+{
+    [Id("edge")]
+    class UnweightedEdge
+    {
+        [Param(0)]
+        private int from;
+
+        [Param(1)]
+        private int to;
+
+        public UnweightedEdge()
+        {
+            this.from = 0;
+            this.to = 0;
+        }
+
+        public UnweightedEdge(int from, int to)
+        {
+            this.from = from;
+            this.to = to;
+        }
+
+        public int getFrom()
+        {
+            return from;
+        }
+
+        public void setFrom(int from)
+        {
+            this.from = from;
+        }
+
+        public int getTo()
+        {
+            return to;
+        }
+
+        public void setTo(int to)
+        {
+            this.to = to;
+        }
+
+    }
+}
